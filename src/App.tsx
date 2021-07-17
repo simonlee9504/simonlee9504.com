38a7fde0a46eea82dfed1deb20601e0fd99701f0
Simon Lee <simonlee9504@gmail.com>
2021-07-17 18:54:24 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { HashRouter, Route, Link } from 'react-router-dom';
+import logo from './logo.svg';
+import './App.css';
+
+const Home = () => (
+	<div>
+		<h2>Home</h2>
+	</div>
+);
+const About = () => (
+	<div>
+		<h2>About</h2>
+	</div>
+);
+
+function App() {
+	return (
+		<HashRouter basename="/">
+			<div>
+				<ul>
+					<li>
+						<Link to="/">Home</Link>
+					</li>
+					<li>
+						<Link to="/about">About</Link>
+					</li>
+				</ul>
+				<hr />
+				<Route exact path="/" component={Home} />
+				<Route path="/about" component={About} />
+			</div>
+		</HashRouter>
+	);
+}
+
+export default App;
